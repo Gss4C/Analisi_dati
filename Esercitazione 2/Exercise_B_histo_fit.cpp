e103d85a01c0ecafd164b1059d2775a9a70cbc21
Jo <jb5nnhy@gmail.com>
2022-08-28 19:28:45 +0200

    inizio punto B

---
+++ Esercitazione 2/Exercise_B_histo_fit.cpp
@@ -0,0 +1,8 @@
+/* 
+Full exercise
+Using ROOT make following steps in order to make a likelihood methods study for datasets "Exercise_Likelihood_n" containing simulations of signals+background
+    A) Plot and fit histograms using various models for background signal  
+    B) Do a likelihood scan of "S" and "B" parameters (counting for signal and background)
+    C) Do a profile likelihood scan of the "S" Parameter
+*/
+using namespace std;
