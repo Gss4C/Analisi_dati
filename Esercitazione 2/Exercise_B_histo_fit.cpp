39a13fa9793e3ee76ddf055ef91b098c02f774dd
Jo <jb5nnhy@gmail.com>
2022-08-29 18:16:11 +0200

    first try not working: idk why

--- Esercitazione 2/Exercise_B_histo_fit.cpp
+++ Esercitazione 2/Exercise_B_histo_fit.cpp
@@ -32,10 +32,10 @@ vector<Double_t> data_from_file(string filename)
     }
     return data;
 }
-Double_t *extended_likelihood(int s, int b, int N, vector<Double_t> x, vector<Double_t> theta)
+Double_t extended_likelihood(int s, int b, int N, vector<Double_t> x, vector<Double_t> theta)
 {
     //Considero che s+b=N
-    TF1 *pdfs_xi=new TF1("pdfs_xi", "-2* ([0]/(sqrt(2*TMath::Pi()*[4]*[4])) * TMath::Exp(-((x - [3]) * (x - [3])) / (2 *[4] *[4])) + [1]/([2]) * TMath::Exp(-x/[2]))");
+    TF1 *pdfs_xi=new TF1("pdfs_xi", "-2* ([0]/(sqrt(2*TMath::Pi()*[4]*[4])) * TMath::Exp(-((x - [3]) * (x - [3])) / (2 *[4] *[4])) + [1]/([2]) * TMath::Exp(-x/[2]))",0,1000);
         /* Readible formula
             -2 *
             (
@@ -43,26 +43,40 @@ Double_t *extended_likelihood(int s, int b, int N, vector<Double_t> x, vector<Do
                 [1]/([2]) * TMath::Exp(-x/[2])
             )
         */
-    pdfs_xi->SetParameters(s,b,theta.at(2),theta.at(0),theta.at(1),N);
+    pdfs_xi->SetParameters( s , b,theta.at(2),theta.at(0),theta.at(1),N);
     Double_t sum = 0;
-    for(int i=0 ; i<=N ; i++)
+    for(int i=0 ; i<N ; i++)
     {
         sum += pdfs_xi->Eval(x.at(i));
     }
-    
+    Double_t output = 2*(s+ b+ N* (TMath::Log(N)-1))+sum;
+    return output;
 }
-//Macro
+
+
 void Exercise_B_histo_fit()
 {
+    TCanvas *c = new TCanvas();
     vector<Double_t> X = data_from_file("exercise_Likelihood_1");
     vector<Double_t> theta = data_from_file("model_1");
-
-    TH1F *h1=histo_fromfile("exercise_Likelihood_1");
-    int N = h1->Integral();
-
-    for(int s=1; s<=N; s++)
-    {
-        int b=N-s;
-        //Da qui dovrebbe partire il calcolo della likelihood, perché in teoria ho tutto il necessario
-    }
+    int N = X.size();
+    
+    // S+B=N case
+        Double_t yi, y[N], s_fin[N];
+        //int b=980;
+        for(int s = 19; s < N; s++)
+        {
+            int b=N-s;
+            yi = extended_likelihood(s, b, N, X, theta);
+            y[s - 19] = yi;
+            s_fin[s - 19] = s;
+        }
+        TGraph *g1=new TGraph(N - 19, s_fin, y);
+        g1->SetTitle("Logaritmo della Likelihood al variare di S");
+        g1->GetXaxis()->SetTitle("S");
+        g1->GetYaxis()->SetTitle("-2*Log(L)");
+        g1->SetMarkerStyle(4);
+        g1->SetMarkerColor(4);
+        g1->Draw("AP");
+    // S+B!=N case
 }
