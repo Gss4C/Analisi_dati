9b0b70c95586e22aef069a64cac91c6122fd0b33
Jo <jb5nnhy@gmail.com>
2022-09-14 18:25:38 +0200

    fine selezione dei tagli

--- final_project/particle.py
+++ final_project/particle.py
@@ -9,7 +9,14 @@ class particle:
         self.flavor=flavor
         self.p4=ROOT.TLorentzVector()
         self.p4.SetPtEtaPhiE(pt,eta,phi,e)
-        
+
+def help_plz(funx="mass_z_range"):
+    if(funx == "mass_z_range"):
+        print("Funzione che fa il confronto fra quel che si vuole considerare nois e quel che è il segnale di interesse, sviluppata con lo scopo di aiutare l'individuazione dei tagli migliori\n Parametri in ingresso:")
+        print("file = Nome del file in lettura \n tree=nome del tree \n var=Selezionare la caratteristica che voglio graficare \n n:bins, min e max \n")
+        print("Parametro OS:\n 0 -> Stampo istogrammi di confronto fra tutti i muoni e i muoni intorno al range \n 1-> Stampo confronto fra tutti i muoni e quelli OS nel range \n 2-> Stampo confronto fra muoni OS nel range e tutti gli SS")
+
+
 def get_particle(tree="tree", var="muontracks", index=0):
     flav_dict={"muontracks":13, "electrons":11}
     p=particle(getattr(tree,var+"_pt")[index],
@@ -49,11 +56,299 @@ def plot_part_mass(file, tree, var="muontracks"):
     #c2.SaveAs("masses_"+var+".JPG")
     return c2, h_mass_os, h_mass_ss
 
-def mass_z_range(tree, var="muontracks", os = 0): #Funzione che fa il confronto fra tutte le particelle e quelle con la massa nel range desiderato (range che deve essere tale da avere eventi prompt)
+
+def mass_z_range(file, tree, var="muontracks_chi2", n_bin=100, min=0, max=100 ,os=0): #Funzione che fa il confronto fra tutte le particelle e quelle con la massa nel range desiderato (range che deve essere tale da avere eventi prompt)
     # var è il nome della caratteristica che voglio vedere: ad esempio muontracks_chi2
-    # se os=0 non faccio distinzione fra os e ss, altrimenti sì
-    if(os==0):
+    # is os=0 I don't distinguish OS and SS, is os==1 I do it
         
+    if(os==0):
+        if(var == "muontracks_chi2"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    chi2 = tree.muontracks_chi2[mi]
+                    h_all.Fill(chi2)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            h_cut.Fill(tree.muontracks_chi2[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
+        if(var == "muontracks_dz"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    dz = tree.muontracks_dz[mi]
+                    h_all.Fill(dz)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            h_cut.Fill(tree.muontracks_dz[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
+        if(var == "muontracks_dB"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    dB = tree.muontracks_dB[mi]
+                    h_all.Fill(dB)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            h_cut.Fill(tree.muontracks_dB[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
+        if(var == "muontracks_isoDeposits"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    isoDeposits = tree.muontracks_isoDeposits[mi]
+                    h_all.Fill(isoDeposits)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            h_cut.Fill(tree.muontracks_isoDeposits[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
     if(os==1):
-    else:
-        print("Error: os parameter shoud be a 0-1 boolean")
+        print("Selecting only OS muons in the mass range")
+        are_os = lambda x,y: x.charge*y.charge<0
+        if(var == "muontracks_chi2"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    chi2 = tree.muontracks_chi2[mi]
+                    h_all.Fill(chi2)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            if(are_os(muons[mi],muons[mj])):
+                                h_cut.Fill(tree.muontracks_chi2[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
+        if(var == "muontracks_dz"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    dz = tree.muontracks_dz[mi]
+                    h_all.Fill(dz)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            if(are_os(muons[mi],muons[mj])):
+                                h_cut.Fill(tree.muontracks_dz[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
+        if(var == "muontracks_dB"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    dB = tree.muontracks_dB[mi]
+                    h_all.Fill(dB)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            if(are_os(muons[mi],muons[mj])):
+                                h_cut.Fill(tree.muontracks_dB[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all
+        if(var == "muontracks_isoDeposits"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_all = ROOT.TH1F(var,var, n_bin,min,max)
+            h_cut = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    isoDeposits = tree.muontracks_isoDeposits[mi]
+                    h_all.Fill(isoDeposits)
+                    for mj in range(mi):
+                        s = (muons[mi].p4 + muons[mj].p4).M()
+                        if(s>80 and s<100):
+                            if(are_os(muons[mi],muons[mj])):
+                                h_cut.Fill(tree.muontracks_isoDeposits[mj])
+            c.Draw()
+            h_cut.SetLineColor(ROOT.kRed)
+            h_cut.Scale(1/h_cut.Integral())
+            h_cut.Draw("hist")
+            h_all.SetLineColor(ROOT.kBlue)
+            h_all.Scale(1/h_all.Integral())
+            h_all.Draw("SAME,hist")
+            return c, h_cut, h_all    
+    if(os==2):
+        print("Selecting only OS muons in the mass range")
+        are_os = lambda x,y: x.charge*y.charge<0
+        if(var == "muontracks_chi2"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_ss = ROOT.TH1F(var,var, n_bin,min,max)
+            h_os = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    for mj in range(mi):
+                        if(are_os(muons[mi],muons[mj])):
+                            s = (muons[mi].p4 + muons[mj].p4).M()
+                            if(s>80 and s<100):
+                                h_os.Fill(tree.muontracks_chi2[mj])
+                        else:
+                            h_ss.Fill(tree.muontracks_chi2[mj])
+            c.Draw()
+            h_os.SetLineColor(ROOT.kRed)
+            h_os.Scale(1/h_os.Integral())
+            h_os.Draw("hist")
+            h_ss.SetLineColor(ROOT.kBlue)
+            h_ss.Scale(1/h_ss.Integral())
+            h_ss.Draw("SAME,hist")
+            return c, h_os, h_ss
+        if(var == "muontracks_dz"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_ss = ROOT.TH1F(var,var, n_bin,min,max)
+            h_os = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    for mj in range(mi):
+                        if(are_os(muons[mi],muons[mj])):
+                            s = (muons[mi].p4 + muons[mj].p4).M()
+                            if(s>80 and s<100):
+                                h_os.Fill(tree.muontracks_dz[mj])
+                        else:
+                            h_ss.Fill(tree.muontracks_dz[mj])
+            c.Draw()
+            h_os.SetLineColor(ROOT.kRed)
+            h_os.Scale(1/h_os.Integral())
+            h_os.Draw("hist")
+            h_ss.SetLineColor(ROOT.kBlue)
+            h_ss.Scale(1/h_ss.Integral())
+            h_ss.Draw("SAME,hist")
+            return c, h_os, h_ss
+        if(var == "muontracks_dB"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_ss = ROOT.TH1F(var,var, n_bin,min,max)
+            h_os = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    for mj in range(mi):
+                        if(are_os(muons[mi],muons[mj])):
+                            s = (muons[mi].p4 + muons[mj].p4).M()
+                            if(s>80 and s<100):
+                                h_os.Fill(tree.muontracks_dB[mj])
+                        else:
+                            h_ss.Fill(tree.muontracks_dB[mj])
+            c.Draw()
+            h_os.SetLineColor(ROOT.kRed)
+            h_os.Scale(1/h_os.Integral())
+            h_os.Draw("hist")
+            h_ss.SetLineColor(ROOT.kBlue)
+            h_ss.Scale(1/h_ss.Integral())
+            h_ss.Draw("SAME,hist")
+            return c, h_os, h_ss
+        if(var == "muontracks_isoDeposits"):
+            file.cd()
+            c=ROOT.TCanvas()
+            h_ss = ROOT.TH1F(var,var, n_bin,min,max)
+            h_os = ROOT.TH1F(var,var, n_bin,min,max)
+            for i in range(tree.GetEntries()):
+                tree.GetEntry(i)
+                muons = get_collection(tree, "muontracks")
+                for mi in range(len(muons)):
+                    for mj in range(mi):
+                        if(are_os(muons[mi],muons[mj])):
+                            s = (muons[mi].p4 + muons[mj].p4).M()
+                            if(s>80 and s<100):
+                                h_os.Fill(tree.muontracks_isoDeposits[mj])
+                        else:
+                            h_ss.Fill(tree.muontracks_isoDeposits[mj])
+            c.Draw()
+            h_os.SetLineColor(ROOT.kRed)
+            h_os.Scale(1/h_os.Integral())
+            h_os.Draw("hist")
+            h_ss.SetLineColor(ROOT.kBlue)
+            h_ss.Scale(1/h_ss.Integral())
+            h_ss.Draw("SAME,hist")
+            return c, h_os, h_ss
